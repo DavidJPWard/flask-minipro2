cc16dec24f06253522576207ee7c95e619d8b460
David Ward <davidward1307@gmail.com>
2022-08-13 15:02:56 +0000

    created databases

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,7 +1,7 @@
 from taskmanager import db
 
 class Category(db.Model):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks= db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
@@ -13,11 +13,11 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Column(db.text, nullable=False)
-    is_urgent = db.Column(db.boolean, default=False, nullable=False)
-    due_date = db.column(db.date, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
