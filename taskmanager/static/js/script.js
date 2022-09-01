430ee59b51fda6bd82d63bf6fccc65475ddb3cea
David Ward <davidward1307@gmail.com>
2022-09-01 14:46:13 +0000

    update init

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,18 @@
   document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
   });
+
+  
